a293d105b45d0fb2a50b11647c8cf5e2eea4fa8d
grekiki2 <gregor1234567890@gmail.com>
2024-06-16 19:56:01 +0200

    auth

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD06olgyVSnP766PRqpPWNad4hynm1sfmI",
+    authDomain: "friendlychat-13caf.firebaseapp.com",
+    projectId: "friendlychat-13caf",
+    storageBucket: "friendlychat-13caf.appspot.com",
+    messagingSenderId: "334455595084",
+    appId: "1:334455595084:web:77d92f6cb6b23df9a814e9",
+    measurementId: "G-MFQ4WC7GS9"
+  };
