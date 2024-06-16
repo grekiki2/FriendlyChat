30e27b6e459a1b0b8cd47ef048b5627b45e7b922
grekiki2 <gregor1234567890@gmail.com>
2024-06-16 18:57:22 +0200

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,12 @@ import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +25,10 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-13caf","appId":"1:334455595084:web:77d92f6cb6b23df9a814e9","storageBucket":"friendlychat-13caf.appspot.com","locationId":"us-central","apiKey":"AIzaSyD06olgyVSnP766PRqpPWNad4hynm1sfmI","authDomain":"friendlychat-13caf.firebaseapp.com","messagingSenderId":"334455595084","measurementId":"G-MFQ4WC7GS9"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI())
   ],
 };
